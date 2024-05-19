5dc831978bbad8b32581d1abfa8ec963df2ae3ef
NehaReddy77 <nehareddy315@gmail.com>
2024-05-19 12:57:45 -0400

    Updates

---
+++ precache-manifest.4cf367681f0b4ca07519b87c8f00c32c.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98ca8fd5620a06f1419eba7939a8b4a",
+    "url": "/Neha/index.html"
+  },
+  {
+    "revision": "83a93a879651453fcfe0",
+    "url": "/Neha/static/css/3.c508d8b9.chunk.css"
+  },
+  {
+    "revision": "6061256068dfc0963756",
+    "url": "/Neha/static/css/4.f0b7a320.chunk.css"
+  },
+  {
+    "revision": "60fb9f5f789ec635499d",
+    "url": "/Neha/static/css/main.d1f649eb.chunk.css"
+  },
+  {
+    "revision": "dbb7b470f7bf46837a61",
+    "url": "/Neha/static/js/2.0dcee576.chunk.js"
+  },
+  {
+    "revision": "83a93a879651453fcfe0",
+    "url": "/Neha/static/js/3.51df96bf.chunk.js"
+  },
+  {
+    "revision": "6061256068dfc0963756",
+    "url": "/Neha/static/js/4.facf34f4.chunk.js"
+  },
+  {
+    "revision": "60fb9f5f789ec635499d",
+    "url": "/Neha/static/js/main.d9ec2d7d.chunk.js"
+  },
+  {
+    "revision": "52fd9041114d9b3d0f47",
+    "url": "/Neha/static/js/runtime-main.9f2afe98.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Neha/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Neha/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4a50b1b3a08c00d85e880457147cb432",
+    "url": "/Neha/static/media/aws-logo.4a50b1b3.png"
+  },
+  {
+    "revision": "d0a70bf39f1215c457e760e598e7bd6f",
+    "url": "/Neha/static/media/cognizant-logo.d0a70bf3.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Neha/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/Neha/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/Neha/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/Neha/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Neha/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/Neha/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "d73bb2bcddad315c67b738e10a345582",
+    "url": "/Neha/static/media/meta_pic.d73bb2bc.png"
+  },
+  {
+    "revision": "7a6c4841a2faec9e578b096408e4cd34",
+    "url": "/Neha/static/media/ncr-logo.7a6c4841.png"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/Neha/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "8bde57f4df480d5b2c66f6af0bb7f62d",
+    "url": "/Neha/static/media/profile.8bde57f4.jpg"
+  },
+  {
+    "revision": "8eef3438717cbacae341a4b5cfbea43f",
+    "url": "/Neha/static/media/profile2.8eef3438.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/Neha/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/Neha/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/Neha/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/Neha/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "c4daca80e09c955cdba1a3a8cbc104ca",
+    "url": "/Neha/static/media/su.c4daca80.png"
+  },
+  {
+    "revision": "9e7ceaae7c1ca27d162a0b61a12a8bd5",
+    "url": "/Neha/static/media/su2.9e7ceaae.png"
+  },
+  {
+    "revision": "c3a2b12964d7d0f89c0d3ac043330eb8",
+    "url": "/Neha/static/media/su3.c3a2b129.png"
+  },
+  {
+    "revision": "0b16d359023a22b371104d7e4a15d32c",
+    "url": "/Neha/static/media/tcs-logo.0b16d359.png"
+  },
+  {
+    "revision": "6c7b47a7b29904919f175780d51a1ec2",
+    "url": "/Neha/static/media/vc.6c7b47a7.png"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/Neha/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "68650cfe6fc9377a581ebbefb7b1a6ad",
+    "url": "/Neha/static/media/woman.68650cfe.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/Neha/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
